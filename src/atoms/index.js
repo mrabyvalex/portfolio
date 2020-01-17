c0053a7237d47dd0445af668dc2e99369db0633b
aby <aby.varghese@nobroker.in>
2020-01-17 11:22:01 +0530

    theming initial commit

--- src/atoms/index.js
+++ src/atoms/index.js
@@ -2,3 +2,5 @@ export { default as PAppBar } from './AppBar';
 export { default as PTypography } from './Typography';
 export { default as PSlide } from './Slide';
 export { default as PuseScrollTrigger } from './UseScrollTrigger';
+export { default as PThemeProvider } from './ThemeProvider';
+export { default as PcreateTheme } from './CreateTheme';
---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const APP_THEME = {
+  palette: {
+    primary: {
+      main: '#009587',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#f44336'
+    },
+    tertiary: {
+      main: '#000'
+    }
+  }
+};
